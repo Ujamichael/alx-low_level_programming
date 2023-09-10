1f7c4bc366a5f52417d44e46b02b2cfcfdac991c
ujamichael <ujahhrmajesti@gmail.com>
2023-09-09 23:32:23 -0700

    base10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int base10;
+
+	for (base10 = 0; base10 <= 9; base10++)
+	{
+		printf("%d", base10);
+	}
+	putchar('\n');
+	return (0);
+}
