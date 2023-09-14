8b233533907f7aa61039b03472bfe46e8b14fa00
ujamichael <ujahhrmajesti@gmail.com>
2023-09-13 20:38:39 -0700

    task ahead

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- entry point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+
+		putchar(x);
+	putchar('\n');
+	/*(return (0);*/
+}
