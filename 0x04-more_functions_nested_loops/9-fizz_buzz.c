b67c95aa4a8424d486b9e6902c56ec26d436cd8a
ujamichael <ujahhrmajesti@gmail.com>
2023-09-15 20:11:48 -0700

    fizzbuzz solved

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -27,7 +27,7 @@ int main(void)
 		}
 		else if (i == 1)
 		{
-			printf(" %d", i);
+			printf("%d", i);
 		}
 		else
 		{
@@ -35,5 +35,6 @@ int main(void)
 		}
 	}
 	printf("\n");
+
 	return (0);
 }
