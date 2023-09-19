ea46e5ab012257310711af7dd8c756ce05f7290b
ujamichael <ujahhrmajesti@gmail.com>
2023-09-19 12:47:14 -0700

    reset var

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - function that resets value of a variable
+ * @n: variable
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
