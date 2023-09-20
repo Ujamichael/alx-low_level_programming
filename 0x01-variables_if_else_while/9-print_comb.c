7c57468108b9d1c455be0c166517e5e5145b1c6b
ujamichael <ujahhrmajesti@gmail.com>
2023-09-20 02:12:59 -0700

    corr

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,13 +3,13 @@
 /**
  * main- entry point
  *
- * Rweturn: 0
+ * Return: 0
  */
 int main(void)
 {
 	int i;
 
-		for (i = 48; i <= 57; i++)
+		for (i = 48; i < 58; i++)
 		{
 			putchar(i);
 			if (i != 57)
