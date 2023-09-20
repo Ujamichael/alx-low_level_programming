a35fe14b6fad110a221fbb18b70a502108fb6931
ujamichael <ujahhrmajesti@gmail.com>
2023-09-19 17:00:03 -0700

    print string reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - print reverse function
+ * @s: string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	while (*s = '\0')
+	{
+		putchar(*s);
+		S--;
+	}
+	putchar('\n');
+}
