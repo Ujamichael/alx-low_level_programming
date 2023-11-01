67206b1487cbaf0d99c79bf048ae54ab1333aa36
ujamichael <ujahhrmajesti@gmail.com>
2023-11-01 09:24:22 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - entry point
+ * @s: original string to be copied
+ * Return: copid string
+ */
+char *_strdup(char *str)
+{
+	size_t i, len;
+	char *copy;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	len = strlen(str);
+	copy = (char *)malloc(len + 1);
+	if (copy != NULL)
+	{
+		for (i = 0; i <= len; i++)
+		{
+			copy[i] = str[i];
+		}
+		return (copy);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
