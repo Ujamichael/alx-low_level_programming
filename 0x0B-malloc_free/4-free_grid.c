500ce09c841cfb73e7d284339ab9bc6156090e7b
ujamichael <ujahhrmajesti@gmail.com>
2023-11-01 21:02:44 -0700

    grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,4 +1,22 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *
+ *  free_grid - entry point
+ *  @height: roll
+ *  @grid: i
+ *  Rreturn: 0
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+		if (grid == NULL)
+		{
+			return;
+		}
+			for (i = 0; i < height; i++)
+			{
+				free(grid[i]);
+			}
+			free(grid);
+}
