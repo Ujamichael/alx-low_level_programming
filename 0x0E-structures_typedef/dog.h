ed2fb70bbcb0c4549febec60260b2d25b663e92e
ujamichael <ujahhrmajesti@gmail.com>
2023-11-06 12:01:49 -0800

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+
+/**
+ * struct dog - structure def
+ * @name: member 1
+ * @age: member 2
+ * @owner: member 3
+ * Return: 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
