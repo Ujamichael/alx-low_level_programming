acc0a155ce78f401ebd1f2254ead11978d06073e
ujamichael <ujahhrmajesti@gmail.com>
2023-11-17 07:03:23 -0800

    singly list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * print_list - function to print all element of list list_t
+ * @list_t: list
+ * @h: pointer to list
+ * Return: element of list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
