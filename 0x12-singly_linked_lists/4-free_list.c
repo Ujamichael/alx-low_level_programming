6b2f18ac6af63270145bb5a10ac95847aa17df0e
ujamichael <ujahhrmajesti@gmail.com>
2023-11-17 19:37:40 -0800

    list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - frees linked list
+ * @head: list_t list to be freed
+ */
+void free_list(list_t *head)
+{
+	list_t *ton;
+
+
+
+	while (head)
+	{
+		ton = head->next;
+		free(head->str);
+		free(head);
+		head = ton;
+	}
+}
